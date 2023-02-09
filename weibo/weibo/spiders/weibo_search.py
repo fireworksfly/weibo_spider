2a4fb829ef0d8727f115a50b11307a520b5353e3
fireworksfly <247715089@qq.com>
2023-02-09 09:59:44 +0800

    实验

--- weibo/weibo/spiders/weibo_search.py
+++ weibo/weibo/spiders/weibo_search.py
@@ -1,5 +1,6 @@
-import scrapy
+import scrapy # scrapy版本2.62, python版本3.8.13
 import sys
+import re
 import os
 from scrapy.utils.project import get_project_settings
 from weibo.utils import util
@@ -33,5 +34,25 @@ class WeiboSearchSpider(scrapy.Spider):
     def parse(self, response, *args):
         base_url = response.meta.get('base_url')
         keyword = response.meta.get('base_url')
-        for content in response.xpath('//div[@action-type="feed_list_item"]//p/text()'):
+        # 获得一页的博文的信息
+        blogs = response.xpath('//div[@action-type="feed_list_item" and @class="card-wrap"]')
+        for every_blog in blogs:
+            # 获取博文的文字内容
+            content = every_blog.xpath('.//div[@class="card"]//p[@class="txt"]').get()
+            content_filter = re.sub('<.*?>', '', content).strip()   # 后期可能考虑换成re.compile
+            social_info = every_blog.xpath('.//div[@class="card-act"]')
+            repost = social_info.xpath('.//ul/li[1]/a').xpath('string(.)').get().strip()
+            if repost == '转发':
+                repost = 0
+            comment = social_info.xpath('.//ul/li[2]/a').xpath('string(.)').get().strip()
+            if comment == '评论':
+                comment = 0
+            like = social_info.xpath('.//ul/li[3]/a').xpath('string(.)').get().strip()
+            if like == '赞':
+                like = 0
+            print(content_filter)
+            print('转发：', repost)
+            print('评论：', comment)
+            print('点赞：', like)
+            print('#############################')
 
